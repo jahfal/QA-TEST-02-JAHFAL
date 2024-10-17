5a327e7632ebe92fb2a2f4690cee019b52588c7d
jahfal <jahfal.lazuardi@sinarmasland.com>
2024-10-17 18:14:03 +0700

    feat: add test case number

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,26 +8,26 @@ describe("ToDo MVC", () => {
     cy.visit("https://todomvc.com/examples/react/dist/");
   });
 
-  it("Verify that the page is loaded successfully", () => {
+  it("MVC-01 Verify that the page is loaded successfully", () => {
     const URL = new visitwebsite();
     URL.verifyURL();
   });
-  it("Add task & verify that the task is correctly added to the list", () => {
+  it("MVC-02 Add task & verify that the task is correctly added to the list", () => {
     addedTask.addTask();
     const markedTasks = new verifyMarkedClasses();
     markedTasks.verifyMarkedTasks();
   });
-  it("Write a test to mark a task as completed & verify that task is visually marked completed", () => {
+  it("MVC-03 Write a test to mark a task as completed & verify that task is visually marked completed", () => {
     addedTask.addTask();
     const markedTasks = new verifyMarkedClass();
     markedTasks.verifyMarkedTasks();
   });
-  it("Delete task and verify that the task is removed to the list", () => {
+  it("MVC-04 Delete task and verify that the task is removed to the list", () => {
     addedTask.addTask();
     cy.xpath("//li[2]//div[1]//button[1]").click({ force: true });
     cy.xpath("//main[@class='main']//li").should("have.length", 2);
   });
-  it.only("Filter task", () => {
+  it.only("MVC-05 Filter task", () => {
     addedTask.addTask();
     cy.xpath("//li[1]//div[1]//input[1]").click();
     cy.xpath("//a[normalize-space()='Active']").click();
